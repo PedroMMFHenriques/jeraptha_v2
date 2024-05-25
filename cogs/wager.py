ac49420c79077bac3a34ca482e268b57dc44d841
Pedro Henriques <pmmfhenriques@gmail.com>
2024-05-26 00:20:34 +0100

    bet system all done (implemented cancel and info)

--- cogs/wager.py
+++ cogs/wager.py
@@ -46,7 +46,9 @@ class Wager(commands.Cog):
 
         # wager: wager id, guild id, quem criou, duração, title, descrição das opções
         wager_id = wagersCol.count_documents({})
-        wagersCol.insert_one({"_id": wager_id, "guild_id": ctx.guild.id, "author_id": ctx.author.id, "settled": False, "end_wager": end_wager_time, "title": title, "option_a": option_a, "option_b": option_b, "option_c": option_c, "option_d": option_d})
+        wagersCol.insert_one({"_id": wager_id, "guild_id": ctx.guild.id, "author_id": ctx.author.id, 
+                              "settled": False, "canceled": False, "end_wager": end_wager_time, "title": title, "winning_option": None,
+                              "option_a": option_a, "option_b": option_b, "option_c": option_c, "option_d": option_d})
 
         wagerRoleId = discord.utils.get(ctx.guild.roles, name=WagerRole).id
         embed = discord.Embed(title="Bet Started: " + title,
@@ -66,7 +68,7 @@ class Wager(commands.Cog):
         embed.set_footer(text="Wager ID: " + str(wager_id),
                          icon_url="https://toppng.com/uploads/thumbnail/hands-holding-playing-cards-royalty-free-vector-clip-hand-holding-playing-cards-clipart-11563240429mbkjvlaujb.png")
 
-        await ctx.send(embed=embed)
+        await ctx.respond(embed=embed)
 
 
 
@@ -78,13 +80,17 @@ class Wager(commands.Cog):
     async def bet(self, ctx: discord.ApplicationContext, wager_id: int, bet_option: str, bet_amount: int):
 
         # Checks
-        wagerCheck = wagersCol.find_one({"_id": wager_id},{"_id": 0, "title": 1, "settled": 1, "end_wager": 1, "option_a": 1, "option_b": 1, "option_c": 1, "option_d": 1})
+        wagerCheck = wagersCol.find_one({"_id": wager_id},{"_id": 0, "title": 1, "settled": 1, "canceled": 1, "end_wager": 1, "option_a": 1, "option_b": 1, "option_c": 1, "option_d": 1})
         if(wagerCheck is None): 
             await ctx.respond("That wager doesn't exist!", ephemeral=True)
             return
         
+        elif(wagerCheck["canceled"] == True): 
+            await ctx.respond("That wager has been canceled!", ephemeral=True)
+            return
+
         elif(wagerCheck["settled"] == True): 
-            await ctx.respond("The wager was already settled!", ephemeral=True)
+            await ctx.respond("That wager was already settled!", ephemeral=True)
             return
         
         elif(wagerCheck["end_wager"] < math.floor(time.time())): 
@@ -129,19 +135,24 @@ class Wager(commands.Cog):
     
     
     #wager settle
-    @wager.command(name="settle", description="Settle the wager.", hidden=True)
+    @wager.command(name="settle", description="Settle a wager.")
     @discord.option("wager_id", description="ID of the wager.", required=True)
     @discord.option("winning_option", description="What option won the bet.", required=True, choices=['option_a', 'option_b', 'option_c', 'option_d'])
     async def settle(self, ctx: discord.ApplicationContext, wager_id: int, winning_option: str):
 
         # Checks
-        wagerCheck = wagersCol.find_one({"_id": wager_id},{"_id": 0, "title": 1, "author_id": 1, "settled": 1, "option_a": 1, "option_b": 1, "option_c": 1, "option_d": 1})
+        wagerCheck = wagersCol.find_one({"_id": wager_id},{"_id": 0, "title": 1, "author_id": 1, "settled": 1, "canceled": 1, "option_a": 1, "option_b": 1, "option_c": 1, "option_d": 1})
         
         if(wagerCheck is None): 
             await ctx.respond("That wager doesn't exist!", ephemeral=True)
             return
+        
+        elif(wagerCheck["canceled"] == True): 
+            await ctx.respond("That wager has been canceled!", ephemeral=True)
+            return
+
         elif(wagerCheck["settled"] == True): 
-            await ctx.respond("The wager was already settled!", ephemeral=True)
+            await ctx.respond("That wager was already settled!", ephemeral=True)
             return
 
         role = discord.utils.get(ctx.author.roles, name=AdminRole)
@@ -156,7 +167,7 @@ class Wager(commands.Cog):
 
         # Settle
         myQuery= {"_id": wager_id}
-        newValues = {'$set': {"settled": True}}
+        newValues = {'$set': {"settled": True, "winning_option": winning_option}}
         wagersCol.update_one(myQuery, newValues)
         
         wagersSub_bettors = wagersSubCol.find({"wager_id": wager_id},{"_id": 0, "member_id": 1, "bet_option": 1, "total_bet": 1})
@@ -196,12 +207,155 @@ class Wager(commands.Cog):
         embed.set_footer(text="Wager ID: " + str(wager_id),
                         icon_url="https://toppng.com/uploads/thumbnail/hands-holding-playing-cards-royalty-free-vector-clip-hand-holding-playing-cards-clipart-11563240429mbkjvlaujb.png")
 
-        await ctx.send(embed=embed)
+        await ctx.respond(embed=embed)
 
 
-    #info: a pool de cada opção e pools + odds
-    #cancel #só admin ou quem criou
-    #history?
+
+    # wager info
+    @wager.command(name="info", description="Get info on the wager.")
+    @discord.option("wager_id", description="ID of the wager.", required=True)
+    async def start(self, ctx: discord.ApplicationContext, wager_id: int):
+
+        wagerCheck = wagersCol.find_one({"_id": wager_id},{"_id": 0, "title": 1, "author_id": 1, "settled": 1, "canceled": 1, "winning_option": 1, "option_a": 1, "option_b": 1, "option_c": 1, "option_d": 1})
+        
+        if(wagerCheck is None): 
+            await ctx.respond("That wager doesn't exist!", ephemeral=True)
+            return
+        
+        wagersSub_bettors = wagersSubCol.find({"wager_id": wager_id},{"_id": 0, "member_id": 1, "bet_option": 1, "total_bet": 1})
+
+        option_wager = {"option_a": 0, "option_b": 0, "option_c": 0, "option_d": 0}
+        option_a_embed = ""
+        option_b_embed = ""
+        option_c_embed = ""
+        option_d_embed = ""
+        for bettor in wagersSub_bettors:
+            option_wager[bettor["bet_option"]] += bettor["total_bet"]
+            string_embed = "<@" + str(bettor["member_id"]) + "> bet " +  str(bettor["total_bet"]) + " coins."
+            if(bettor["bet_option"] == "option_a"): option_a_embed += string_embed
+            elif(bettor["bet_option"] == "option_b"): option_b_embed += string_embed
+            elif(bettor["bet_option"] == "option_c"): option_c_embed += string_embed
+            elif(bettor["bet_option"] == "option_d"): option_d_embed += string_embed
+        
+        total_bet = option_wager["option_a"] + option_wager["option_b"] + option_wager["option_c"] + option_wager["option_d"]
+
+        if(option_wager["option_a"] == 0): option_a_odds = "N/A"
+        else: option_a_odds = 0.99 * total_bet / option_wager["option_a"]
+
+        if(option_wager["option_b"] == 0): option_b_odds = "N/A"
+        else: option_b_odds = 0.99 * total_bet / option_wager["option_b"]
+
+        if(option_wager["option_c"] == 0): option_c_odds = "N/A"
+        else: option_c_odds = 0.99 * total_bet / option_wager["option_c"]
+
+        if(option_wager["option_d"] == 0): option_d_odds = "N/A"
+        else: option_d_odds = 0.99 * total_bet / option_wager["option_d"]
+        
+        description_embed = ""
+        if(wagerCheck["canceled"]): description_embed = "**CANCELED**\n"
+        elif(wagerCheck["settled"]): description_embed = "**SETTLED**\nWinning option: **" + wagerCheck["winning_option"] + "**\n"
+        
+        description_embed += "**" + wagerCheck["option_a"] + "**: " + str(option_wager["option_a"]) + " coins, " + str(option_a_odds) + " odds\n"
+        description_embed += "**" + wagerCheck["option_b"] + "**: " + str(option_wager["option_b"]) + " coins, " + str(option_b_odds) + " odds\n"
+        if(wagerCheck["option_c"] != ""):
+            description_embed += "**" + wagerCheck["option_c"] + "**: " + str(option_wager["option_c"]) + " coins, " + str(option_c_odds) + " odds\n"
+        if(wagerCheck["option_d"] != ""):
+            description_embed += "**" + wagerCheck["option_d"] + "**: " + str(option_wager["option_d"]) + " coins, " + str(option_d_odds) + " odds\n"
+
+        embed = discord.Embed(title="Bet Info: " + wagerCheck["title"],
+                      description=description_embed,
+                      colour=0x009900,
+                      timestamp=datetime.now())
+
+        if(option_wager["option_a"] == 0): option_a_embed_name = "Nobody bet on **" + wagerCheck["option_a"] + "**!"
+        else: option_a_embed_name = "Bettors on **" + wagerCheck["option_a"] + "**:"
+
+        if(option_wager["option_b"] == 0): option_b_embed_name = "Nobody bet on **" + wagerCheck["option_b"] + "**!"
+        else: option_b_embed_name = "Bettors on **" + wagerCheck["option_b"] + "**:"
+
+        if(wagerCheck["option_c"] != ""):
+            if(option_wager["option_c"] == 0): option_c_embed_name = "Nobody bet on **" + wagerCheck["option_c"] + "**!"
+            else: option_c_embed_name = "Bettors on **" + wagerCheck["option_c"] + "**:"
+        
+        if(wagerCheck["option_d"] != ""):
+            if(option_wager["option_d"] == 0): option_d_embed_name = "Nobody bet on **" + wagerCheck["option_d"] + "**!"
+            else: option_d_embed_name = "Bettors on **" + wagerCheck["option_d"] + "**:"
+
+        embed.add_field(name=option_a_embed_name,
+                        value=option_a_embed,
+                        inline=False)
+        
+        embed.add_field(name=option_b_embed_name,
+                        value=option_b_embed,
+                        inline=False)
+        
+        if(wagerCheck["option_c"] != ""):
+            embed.add_field(name=option_c_embed_name,
+                            value=option_c_embed,
+                            inline=False)
+
+        
+        if(wagerCheck["option_d"] != ""):
+            embed.add_field(name=option_d_embed_name,
+                            value=option_d_embed,
+                            inline=False)
+
+        embed.set_footer(text="Wager ID: " + str(wager_id),
+                         icon_url="https://toppng.com/uploads/thumbnail/hands-holding-playing-cards-royalty-free-vector-clip-hand-holding-playing-cards-clipart-11563240429mbkjvlaujb.png")
+
+        await ctx.respond(embed=embed)
+
+
+
+    #wager cancel
+    @wager.command(name="cancel", description="Cancel a wager.")
+    @discord.option("wager_id", description="ID of the wager.", required=True)
+    async def cancel(self, ctx: discord.ApplicationContext, wager_id: int):
+
+        # Checks
+        wagerCheck = wagersCol.find_one({"_id": wager_id},{"_id": 0, "title": 1, "author_id": 1, "settled": 1, "canceled": 1, "option_a": 1, "option_b": 1, "option_c": 1, "option_d": 1})
+        
+        if(wagerCheck is None): 
+            await ctx.respond("That wager doesn't exist!", ephemeral=True)
+            return
+        
+        elif(wagerCheck["canceled"] == True): 
+            await ctx.respond("That wager has been canceled!", ephemeral=True)
+            return
+        
+        elif(wagerCheck["settled"] == True): 
+            await ctx.respond("That wager was already settled!", ephemeral=True)
+            return
+
+        role = discord.utils.get(ctx.author.roles, name=AdminRole)
+        if(role is None and wagerCheck["author_id"] != ctx.author.id):
+            await ctx.respond("You didn't start this bet, so you can't cancel!", ephemeral=True)
+            return
+
+
+        # Cancel and return bets
+        myQuery= {"_id": wager_id}
+        newValues = {'$set': {"canceled": True,}}
+        wagersCol.update_one(myQuery, newValues)
+        
+        wagersSub_bettors = wagersSubCol.find({"wager_id": wager_id},{"_id": 0, "member_id": 1, "bet_option": 1, "total_bet": 1})
+
+        for bettor in wagersSub_bettors:
+            myQuery= {"member_id": int(bettor["member_id"]), "guild_id": ctx.guild.id}
+            newValues = {'$inc': {'coins': bettor["total_bet"]}}
+            usersCol.update_one(myQuery, newValues)
+
+        # Embed
+        wagerRoleId = discord.utils.get(ctx.guild.roles, name=WagerRole).id
+        embed = discord.Embed(title="Bet Canceled: " + wagerCheck["title"],
+                      description="<@&" + str(wagerRoleId) + ">\n<@" + str(ctx.author.id) + "> canceled the bet!\nThe bets have been returned.",
+                      colour=0x009900,
+                      timestamp=datetime.now())
+
+        embed.set_footer(text="Wager ID: " + str(wager_id),
+                        icon_url="https://toppng.com/uploads/thumbnail/hands-holding-playing-cards-royalty-free-vector-clip-hand-holding-playing-cards-clipart-11563240429mbkjvlaujb.png")
+
+        await ctx.respond(embed=embed)
 
 
 def setup(bot):
