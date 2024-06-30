842cc9138af819ba7f26bcf360cecfc3234ffdcf
Pedro Henriques <pmmfhenriques@gmail.com>
2024-06-30 12:45:51 +0100

    forgor author_id

--- cogs/wager.py
+++ cogs/wager.py
@@ -419,7 +419,7 @@ class Wager(commands.Cog):
 
         description_embed = ""
         for wager in wagerCheck:
-            description_embed += "[ID " + str(wager["_id"]) + "] **" + wager["title"] + "** by " + wager["author"]
+            description_embed += "[ID " + str(wager["_id"]) + "] **" + wager["title"] + "** by " + wager["author_id"]
             if(option == "All"):
                 if(not wager["settled"] and not wager["canceled"]): description_embed += ", [OPEN]"
                 elif(wager["settled"]): description_embed += ", [SETTLED]: " + wager["winning_option"]
