cf1840faf9fc8b9b3d1ada8f84f0f68acb88b0d1
Pedro Henriques <pmmfhenriques@gmail.com>
2024-07-02 23:40:50 +0100

    Update beetdle.py
    Co-Authored-By: Toalha <48457387+toalha@users.noreply.github.com>

--- cogs/beetdle.py
+++ cogs/beetdle.py
@@ -155,12 +155,12 @@ class Beetdle(commands.Cog):
 
         else: # Incorrect word, continue game
             # Stores the list of unused letters (0: wrong; 1: unused; 2: wrong place; 3: correct)
-            unused_letters = {'a': 1, 'b': 1, 'c': 1, 'd': 1, 'e': 1, 'f': 1, 'g': 1,
-                            'h': 1, 'i': 1, 'j': 1, 'k': 1, 'l': 1, 'm': 1, 'n': 1,
-                            'o': 1, 'p': 1, 'q': 1, 'r': 1, 's': 1, 't': 1, 'u': 1,
-                            'v': 1, 'w': 1, 'x': 1, 'y': 1, 'z': 1}
+            unused_letters = {'A': 1, 'B': 1, 'C': 1, 'D': 1, 'E': 1, 'F': 1, 'G': 1,
+                            'H': 1, 'I': 1, 'J': 1, 'K': 1, 'L': 1, 'M': 1, 'N': 1,
+                            'O': 1, 'P': 1, 'Q': 1, 'R': 1, 'S': 1, 'T': 1, 'U': 1,
+                            'V': 1, 'W': 1, 'X': 1, 'Y': 1, 'Z': 1}
             # Saves the correct letters so far, to facilitate guessing
-            correct_letters=["_", "_", "_", "_", "_"]
+            correct_letters=["__ __", "__ __", "__ __", "__ __", "__ __"]
             
             word_count = {}
             for letter in word:
@@ -258,9 +258,7 @@ class Beetdle(commands.Cog):
         temp_guess_list = guesses_print.split("\n")
         for words_formatted in temp_guess_list[:-1]:
             letters_formatted = words_formatted.split(" ")
-            print(letters_formatted)
             for iterator in range(5):
-                print(letters_formatted[iterator])
                 temp_letter = letters_formatted[iterator]
                 if(len(temp_letter) == 5):
                     if(temp_letter[0] == "*"):
